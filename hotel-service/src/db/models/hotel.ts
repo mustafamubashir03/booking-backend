6224d50bc5fdedeb8f21a80b02d866623d59dd45
Mustafa <mustafamubashir87@gmail.com>
2026-04-11 23:34:16 +0500

    Added ratings migration and tested db queries

---
+++ hotel-service/src/db/models/hotel.ts
@@ -0,0 +1,56 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import sequelize from "./sequelize";
+
+class Hotel extends Model<InferAttributes<Hotel>, InferCreationAttributes<Hotel>>{
+    declare id: CreationOptional<number>;
+    declare name: string;
+    declare address: string;
+    declare location: string;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+    declare rating: number;
+    declare rating_count: number;
+}
+
+
+Hotel.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        rating: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        rating_count: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
+    },
+    {
+        sequelize: sequelize, 
+        tableName: "hotels",
+        underscored:true,
+        timestamps:true
+    }
+);
+
+export default Hotel;
--- hotel-service/src/server.ts
+++ hotel-service/src/server.ts
@@ -5,6 +5,8 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import sequelize from './db/models/sequelize';
+import Hotel from './db/models/hotel';
 const app = express();
 
 app.use(express.json());
@@ -26,7 +28,33 @@ app.use(appErrorHandler);
 app.use(genericErrorHandler);
 
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, async() => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
+    // try{
+    //     await sequelize.authenticate();
+    //     logger.info("Database connection has been established")
+    //     const hotel = await Hotel.create({
+    //         name:"Hotel AL Baik",
+    //         location:"Karachi, Pakistan",
+    //         address:"Gulshan e Maymar",
+    //         rating:3,
+    //         rating_count:10
+    //     })
+    //     logger.info("Hotel created successfully",hotel.toJSON())
+
+    // }catch{
+    //     logger.error("Failed to create hotel")
+
+    // }
+    try{
+        await sequelize.authenticate();
+        logger.info("Database connection has been established")
+        const hotels = await Hotel.findAndCountAll()
+        logger.info("Hotel fetched successfully",hotels)
+
+    }catch{
+        logger.error("Failed to create hotel")
+
+    }
 });
